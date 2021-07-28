b4457e3b92f5035e5d48a99eb4d82aa3e6a245ca
DW1219 <ymg886@naver.com>
2021-07-29 00:20:51 +0900

    first commit

---
+++ src/components/views/d/memo.js
@@ -0,0 +1,34 @@
+/*****
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ *
+<svg width="360" height="380" viewBox="0 0 360 380" fill="none" xmlns="http://www.w3.org/2000/svg">
+
+
+<circle class="solarToGrid" r="6" fill="white" fill-opacity="0.9"></circle>
+<circle class="solarToGrid" r="20" fill="white" fill-opacity="0.4"></circle>
+<circle class="solarToHome" r="6" fill="white" fill-opacity="0.9"></circle>
+<circle class="solarToHome" r="20" fill="white" fill-opacity="0.4"></circle>
+<circle class="solarToCharge" r="6" fill="white" fill-opacity="0.9"></circle>
+<circle class="solarToCharge" r="20" fill="white" fill-opacity="0.4"></circle>
+
+
+<animate
+xlinkHref="#solar_blur"
+attributeName="radius"
+from="3"
+to="1"
+dur="2s"
+begin="0s" repeatCount="indefinite"
+/>
+
+
+
+
+ */
